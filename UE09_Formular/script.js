6509ccc7d38252c9d4951e375626e08e946b9cdb
Clemens <mail@clemens-rumpfhuber.at>
2020-01-29 11:19:15 +0100

    Updated UE09

--- UE09_Formular/script.js
+++ UE09_Formular/script.js
@@ -1,9 +1,9 @@
 /* Clemens Rumpfhuber 3AHIT */
 
 function generateNumbers() {
-    let a = parseInt(document.getElementById("number_a").value);
-    let b = parseInt(document.getElementById("number_b").value);
-    let c = parseInt(document.getElementById("amount").value);
+    let a = parseInt(window.document.generator.number_a.value);
+    let b = parseInt(window.document.generator.number_b.value);
+    let c = parseInt(window.document.generator.amount.value);
     let txt = "";
     for (let i = 0; i < c; i++) txt += a + Math.round((b-1)*Math.random()) + (i+1 === c ? "" : ", ");
     document.getElementById('output').innerText = txt;
