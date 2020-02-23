000f240069e85d0c0cb4bca377481f54975bc97c
Clemens <mail@clemens-rumpfhuber.at>
2020-02-23 17:55:59 +0100

    Restructured Project

--- UE01_Quadrate/script.js
+++
@@ -1,5 +0,0 @@
-/* Clemens Rumpfhuber 3AHIT */
-
-for (var i = 0; i < 10; i++) {
-    document.write('<tr>', "<td>", i, "</td>", "<td>", i * i, "</td>", "</tr>");
-}
--- UE08_Boxenstapel/script.js
+++
@@ -1,8 +0,0 @@
-/* Clemens Rumpfhuber 3AHIT */
-
-var b = true;
-
-function modify(elementID) {
-    document.getElementById(elementID).style = "background-color: " + ((b = !b) ? "orange" : "yellow");
-    document.getElementById(elementID).innerText = (b ? "CLACK" : "CLICK");
-}
