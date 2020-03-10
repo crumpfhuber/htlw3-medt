116baeeaa0e7805a44b12157b47b9b1cd647dc62
Clemens <mail@clemens-rumpfhuber.at>
2020-03-10 16:54:53 +0100

    Updated BSP14

--- JavaScript/BSP14_KlickKluckKlick/script.js
+++ JavaScript/BSP14_KlickKluckKlick/script.js
@@ -1,8 +1,9 @@
-function textChanger(elementId) {
+function changeText(elementId) {
     var el = document.getElementById(elementId);
     switch (el.innerText) {
         case "Klick": el.innerHTML = "Klack"; break;
         case "Klack": el.innerHTML = "Kluck"; break;
         case "Kluck": el.innerHTML = "Klick"; break;
+        default: el.innerHTML = "Klick"; break;
     }
 }
