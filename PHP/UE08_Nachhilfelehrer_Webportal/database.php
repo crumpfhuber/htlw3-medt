7c7407bee8798f19df137b046b7f69ccb6468c0c
Clemens <mail@clemens-rumpfhuber.at>
2020-06-05 23:24:00 +0200

    Update UE08 (Update Contact, Rewrite File Storage)

--- PHP/UE08_Nachhilfelehrer_Webportal/database.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/database.php
@@ -11,25 +11,44 @@ function getAllNews() {
     return $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function addFile($file_name, $mime_type, $file, $user_id) {
+function addFile($file_name, $mime_type, $file) {
     global $pdo;
     $id = '';
     for ($i = 0; $i < 16; $i++) $id .= chr(rand(97, 122));
     $file = base64_encode($file);
-    $statement = $pdo->prepare('INSERT INTO `storage` (`id`, `mime_type`, `file`, `name`, `owner`) VALUES (:id, :mime_type, :file, :name, :owner)');
-    $statement->bindParam(":id", $id);
+    $statement = $pdo->prepare('INSERT INTO `file` (`sid`, `mime_type`, `file`, `name`) VALUES (:sid, :mime_type, :file, :name)');
+    $statement->bindParam(":sid", $id);
     $statement->bindParam(":mime_type", $mime_type);
     $statement->bindParam(":file", $file);
     $statement->bindParam(":name", $file_name);
-    $statement->bindParam(":owner", $user_id);
     $statement->execute();
-    return $statement->fetch();
+    return getFileId($pdo->lastInsertId())['sid'];
+}
+
+function getFileId($file_id) {
+    global $pdo;
+    $statement = $pdo->prepare('SELECT `sid` FROM `file` WHERE `id` = :id');
+    $statement->bindParam(":id", $file_id);
+    $statement->execute();
+    return $statement->fetch(PDO::FETCH_ASSOC);
 }
 
 function getFile($file_id) {
     global $pdo;
-    $statement = $pdo->prepare('SELECT * FROM `storage` WHERE `id` = :id');
+    $statement = $pdo->prepare('SELECT * FROM `file` WHERE `sid` = :id');
     $statement->bindParam(":id", $file_id);
     $statement->execute();
     return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+function addContactRequest($firstname, $lastname, $email, $content, $file_id=NULL) {
+    global $pdo;
+    $statement = $pdo->prepare('INSERT INTO `contact`(`firstname`, `lastname`, `email`, `content`, `attachment`) VALUES (:firstname, :lastname, :email, :content, :attachment)');
+    $statement->bindParam(":firstname", $firstname);
+    $statement->bindParam(":lastname", $lastname);
+    $statement->bindParam(":email", $email);
+    $statement->bindParam(":content", $content);
+    $statement->bindParam(":attachment", $file_id);
+    $statement->execute();
+    return $statement->fetch();
 }
--- PHP/UE08_Nachhilfelehrer_Webportal/pages/contact.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/pages/contact.php
@@ -1,3 +1,53 @@
 <?php
+if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['textarea'])) {
+    $file = NULL;
+    if (isset($_FILES['file']) && $_FILES['file']['tmp_name'] != "")
+        $file = addFile($_FILES['file']['name'], $_FILES['file']['type'], file_get_contents($_FILES['file']['tmp_name']));
+    addContactRequest($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['textarea'], $file);
+    echo '<script>  M.toast({html: \'Das Formular wurde erfolgreich abgesendet. Vielen Dank!\'})</script>';
+}
+?>
 
-echo "<h1>hier kontaktform</h1>";
+<div class="row">
+    <form class="col s12" method="post" enctype="multipart/form-data">
+
+        <div class="row">
+            <div class="input-field col s6">
+                <input id="firstname" name="firstname" type="text" class="validate" required>
+                <label for="firstname">Vorname</label>
+            </div>
+
+            <div class="input-field col s6">
+                <input id="lastname" name="lastname" type="text" class="validate" required>
+                <label for="lastname">Nachname</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="email" name="email" type="email" class="validate" required>
+                <label for="email">E-Mail Adresse</label>
+            </div>
+
+
+            <div class="input-field col s12">
+                <textarea id="textarea" name="textarea" class="materialize-textarea validate" required></textarea>
+                <label for="textarea">Anfrage</label>
+            </div>
+
+            <div class="file-field col s12">
+                <div class="btn">
+                    <span>Datei hochladen</span>
+                    <input type="file" name="file">
+                </div>
+                <div class="file-path-wrapper">
+                    <input class="file-path validate" type="text" placeholder="Eine Datei kann angehängt werden.">
+                </div>
+            </div>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit" name="action">Absenden
+            <i class="material-icons right">send</i>
+        </button>
+    </form>
+</div>
