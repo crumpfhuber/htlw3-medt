60d4a0be20d22e70624c63176e42321ac27047fa
Clemens <mail@clemens-rumpfhuber.at>
2020-06-05 10:46:23 +0200

    Update UE08 (Update News)

--- PHP/UE08_Nachhilfelehrer_Webportal/pages/news.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/pages/news.php
@@ -1,3 +1,17 @@
+<div class="row">
 <?php
-
-echo "<h1>hier aktuelles</h1>";
+$news = getAllNews();
+foreach ($news as $obj) { ?>
+        <div class="col s12 m6">
+            <div class="card">
+                <div class="card-image">
+                    <img src="/file/<?php echo $obj['image']; ?>">
+                    <span class="card-title"><?php echo $obj['headline']; ?></span>
+                </div>
+                <div class="card-content">
+                    <p><?php echo $obj['content']; ?></p>
+                </div>
+            </div>
+        </div>
+<?php } ?>
+</div>
