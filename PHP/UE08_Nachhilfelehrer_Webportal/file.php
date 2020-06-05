ce37892a9a2adaa6ca4c71bea1a65f971215f650
Clemens <mail@clemens-rumpfhuber.at>
2020-06-05 22:23:53 +0200

    Update UE08 (Add 404 File Error Message)

--- PHP/UE08_Nachhilfelehrer_Webportal/file.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/file.php
@@ -5,6 +5,9 @@ $tabs = explode('/', $_GET['page']);
 
 $file = getFile($tabs[0]);
 
+if ($file == NULL)
+    die("An error occurred. Please contact the webmaster!");
+
 header('Content-Type: ' . $file['mime_type']);
 
 echo base64_decode($file['file']);
