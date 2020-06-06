20c3be24594a8db28d03d5590a115c34baf17a1a
Clemens <mail@clemens-rumpfhuber.at>
2020-06-06 09:48:46 +0200

    Update UE08 (Add Logout Site)

--- PHP/UE08_Nachhilfelehrer_Webportal/fileup.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/fileup.php
@@ -11,4 +11,4 @@ require_once 'database.php';
 
 print_r($_FILES['fileUpload']);
 
-echo addFile($_FILES['fileUpload']['name'], $_FILES['fileUpload']['type'], file_get_contents($_FILES['fileUpload']['tmp_name']));
+echo addFile($_FILES['fileUpload']['name'], $_FILES['fileUpload']['type'], file_get_contents($_FILES['fileUpload']['tmp_name']), "Uploaded via fileup.php");
---
+++ PHP/UE08_Nachhilfelehrer_Webportal/pages/logout.php
@@ -0,0 +1,4 @@
+<?php
+session_destroy();
+
+die('<meta http-equiv="refresh" content="0; URL=/home">');
