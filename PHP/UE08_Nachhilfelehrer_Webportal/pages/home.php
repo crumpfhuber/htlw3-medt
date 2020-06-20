3c12fe8090f90cab9d5698d0e15c32b31b8920f2
Clemens <mail@clemens-rumpfhuber.at>
2020-06-20 17:02:29 +0200

    Update UE08 (Add Landing Page)

--- PHP/UE08_Nachhilfelehrer_Webportal/pages/home.php
+++ PHP/UE08_Nachhilfelehrer_Webportal/pages/home.php
@@ -1,3 +1,85 @@
-<?php
+<!-- https://bootsnipp.com/materialize/snippets/zDopd -->
+<div class="slider">
+    <ul class="slides">
+        <li>
+            <img src="/file/dyqqlfkmdzfuzkah">
+            <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="/file/fyuokdllggifqjsf">
+            <div class="caption left-align">
+                <h3>Left Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="/file/mmicosegzvfvuaxq">
+            <div class="caption right-align">
+                <h3>Right Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+        <li>
+            <img src="/file/avpnqdcgycbgfsdl">
+            <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+            </div>
+        </li>
+    </ul>
+</div>
 
-echo "<h1>hier home</h1>";
+<!-- https://bootsnipp.com/materialize/snippets/klbxW -->
+<div class="row" style="margin-top: 3vh;">
+    <div class="col xl12 l12 m12 s12">
+        <div class="card-panel center teal">
+          <span class="white-text">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.          </span>
+        </div>
+    </div>
+
+    <div class="col xl4 l4 m12 12">
+        <div class="card-panel center promo blue-grey darken-3 white-text">
+            <i class="material-icons">grade</i>
+            <p class="promo-caption">Example Field 1</p>
+            <p class="light center">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.            </p>
+        </div>
+    </div>
+    <div class="col xl4 l4 m12 12">
+        <div class="card-panel center promo blue-grey darken-3 white-text">
+            <i class="material-icons">free_breakfast</i>
+            <p class="promo-caption">Example Field 2</p>
+            <p class="light center">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.            </p>
+        </div>
+    </div>
+    <div class="col xl4 l4 m12 12">
+        <div class="card-panel center promo blue-grey darken-3 white-text">
+            <i class="material-icons">school</i>
+            <p class="promo-caption">Example Field 3</p>
+            <p class="light center">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.            </p>
+        </div>
+    </div>
+
+    <div class="col xl12 l12 m12 s12">
+        <div class="card-panel center blue">
+          <span class="white-text">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.          </span>
+        </div>
+    </div>
+
+    <div class="col xl12 l12 m12 s12" style="margin-top: 3vw;">
+        <iframe style="width: 100%; height: 60vh;" src="https://www.youtube.com/embed/mcixldqDIEQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $('.slider').slider();
+    });
+</script>
